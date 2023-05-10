8f7b82e32520f734a484c6bda78ed61e19ad3393
LeonardoXavier1 <leonardomx500@gmail.com>
2023-05-10 01:03:44 -0300

    certinho 2

--- back.php
+++ back.php
@@ -1,9 +1,8 @@
     <?php
-
-function validaCPF($cpf) {
+function CPFcalc($cpf) {
     $cpf = preg_replace('/[^0-9]/', '', $cpf);
     if (strlen($cpf) != 11) {
-        return false;
+        return "O CPF deve conter exatamente 11 dígitos.";
     }
     for ($t = 9; $t < 11; $t++) {
         for ($d = 0, $c = 0; $c < $t; $c++) {
@@ -11,36 +10,94 @@ function validaCPF($cpf) {
         }
         $d = ((10 * $d) % 11) % 10;
         if ($cpf[$c] != $d) {
-            return false;
+            return "O CPF fornecido não é válido.";
         }
     }
     return true;
 }
-    
-if(isset($_POST['nome'],$_POST['email'],$_POST['cpf'])){
+
+$errors = array();
+$fields = array('nome', 'email', 'cpf');
+
+if (isset($_POST['nome'], $_POST['email'], $_POST['cpf'])) {
+   if(($_POST['nome'] != "") && ($_POST['email'] != "" )&& ($_POST['cpf'] != "" )){
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $cpf = $_POST['cpf'];
-    
-    // Validar campo vazio
-    if($email == "" || $cpf == "" || $nome == ""){
-        echo "Campos vazios";
+
+    // Validar campo de email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "O email fornecido não é válido.";
+    }
+
+    // Validar campo de cpf
+    $cpfValidationResult = CPFcalc($cpf);
+    if ($cpfValidationResult !== true) {
+        $errors[] = $cpfValidationResult;
+    }
+
+    // Validar campo de nome
+    if (!preg_match('/^[a-zA-Z]{3,}$/', $nome)) {
+        $errors[] = "O nome fornecido deve conter no mínimo 3 letras.";
+    }
+
+    if (empty($errors)) {
+        echo "Todos os campos são válidos.";
     } else {
-        // Validar campo de email
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            echo "O email fornecido não é válido.";
-        } else {
-            // Validar campo de cpf
-            if(!validaCPF($cpf)){
-                echo "O CPF fornecido não é válido.";
-            } else {
-                // Validar campo de nome
-                if(!preg_match('/^[a-zA-Z]{3,}$/', $nome)){
-                    echo "O nome fornecido deve conter no mínimo 3 letras.";
-                } else {
-                    echo "Todos os campos são válidos.";
-                }
-            }
+        echo "Erros encontrados:<br>";
+        foreach ($errors as $error) {
+            echo $error . "<br>";
         }
     }
+   } 
 }
+
+
+
+
+
+
+// function CPFcalc($cpf) {
+//     $cpf = preg_replace('/[^0-9]/', '', $cpf);
+//     if (strlen($cpf) != 11) {
+//         return false;
+//     }
+//     for ($t = 9; $t < 11; $t++) {
+//         for ($d = 0, $c = 0; $c < $t; $c++) {
+//             $d += $cpf[$c] * (($t + 1) - $c);
+//         }
+//         $d = ((10 * $d) % 11) % 10;
+//         if ($cpf[$c] != $d) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+    
+// if(isset($_POST['nome'],$_POST['email'],$_POST['cpf'])){
+//     $nome = $_POST['nome'];
+//     $email = $_POST['email'];
+//     $cpf = $_POST['cpf'];
+    
+//     // Validar campo vazio
+//     if($email == "" || $cpf == "" || $nome == ""){
+//         echo "Campos vazios";
+//     } else {
+//         // Validar campo de email
+//         if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+//             echo "O email fornecido não é válido.";
+//         } else {
+//             // Validar campo de cpf
+//             if(!CPFcalc($cpf)){
+//                 echo "O CPF fornecido não é válido.";
+//             } else {
+//                 // Validar campo de nome
+//                 if(!preg_match('/^[a-zA-Z]{3,}$/', $nome)){
+//                     echo "O nome fornecido deve conter no mínimo 3 letras.";
+//                 } else {
+//                     echo "Todos os campos são válidos.";
+//                 }
+//             }
+//         }
+//     }
+// }
